baa73d1e110ba3eb377856e214e93c62a6456aed
nmamie <noah.mamie@uzh.ch>
2023-11-07 10:02:02 +0100

    clean up

--- assignments/03/preprocess_data.sh
+++ assignments/03/preprocess_data.sh
@@ -24,7 +24,7 @@ perl moses_scripts/train-truecaser.perl --model $data/preprocessed/tm.$src --cor
 perl moses_scripts/train-truecaser.perl --model $data/preprocessed/tm.$tgt --corpus $data/preprocessed/train.$tgt.p
 
 # apply truecase models to splits
-cat $data/preprocessed/train.$src.p | perl moses_scripts/truecase.perl --model $data/preprocessed/tm.$src > $data/preprocessed/train.$src 
+cat $data/preprocessed/train.$src.p | perl moses_scripts/truecase.perl --model $data/preprocessed/tm.$src > $data/preprocessed/train.$src
 cat $data/preprocessed/train.$tgt.p | perl moses_scripts/truecase.perl --model $data/preprocessed/tm.$tgt > $data/preprocessed/train.$tgt
 
 # prepare remaining splits with learned models
@@ -40,24 +40,6 @@ rm $data/preprocessed/train.$tgt.p
 
 # preprocess all files for model training
 # python preprocess_bpe_autoencoder.py --target-lang $tgt --source-lang $src --dest-dir $data/prepared/ --train-prefix $data/preprocessed/train --valid-prefix $data/preprocessed/valid --test-prefix $data/preprocessed/test --tiny-train-prefix $data/preprocessed/tiny_train --input $data/preprocessed/train.fr $data/preprocessed/train.en --output $data/preprocessed/bpe_codes --write-vocabulary $data/preprocessed/vocab.fr $data/preprocessed/vocab.en --threshold-src 1 --threshold-tgt 1 --num-words-src 4000 --num-words-tgt 4000 --symbols 10000 --train-bpe --dropout 0.1 --num-workers 16
-python preprocess_bpe_autoencoder.py --target-lang $tgt --source-lang $src --dest-dir $data/prepared/ --train-prefix $data/preprocessed/train --valid-prefix $data/preprocessed/valid --test-prefix $data/preprocessed/test --tiny-train-prefix $data/preprocessed/tiny_train --input $data/preprocessed/train.fr $data/preprocessed/train.en --output $data/preprocessed/bpe_codes --output-bpe $data/preprocessed/train.bpe.fr $data/preprocessed/train.bpe.en --vocabulary $data/preprocessed/vocab.fr $data/preprocessed/vocab.en --write-vocabulary $data/preprocessed/vocab.fr $data/preprocessed/vocab.en --threshold-src 1 --threshold-tgt 1 --num-words-src 4000 --num-words-tgt 4000 --symbols 10000 --train-bpe --dropout 0.1 --num-workers 16
+python preprocess_bpe_autoencoder.py --target-lang $tgt --source-lang $src --dest-dir $data/prepared/ --train-prefix $data/preprocessed/train --valid-prefix $data/preprocessed/valid --test-prefix $data/preprocessed/test --tiny-train-prefix $data/preprocessed/tiny_train --input $data/preprocessed/train.fr $data/preprocessed/train.en --output $data/preprocessed/bpe_codes --output-bpe $data/preprocessed/train.bpe.fr $data/preprocessed/train.bpe.en --vocabulary $data/preprocessed/vocab.fr $data/preprocessed/vocab.en --write-vocabulary $data/preprocessed/vocab.fr $data/preprocessed/vocab.en --threshold-src 1 --threshold-tgt 1 --num-words-src 4000 --num-words-tgt 4000 --symbols 4000 --train-bpe --dropout 0 --num-workers 16
 
 echo "done!"
-
-
-# python preprocess_bpe_autoencoder.py \
-# --target-lang fr \
-# --source-lang en \
-# --dest-dir data/en-fr/prepared/ \
-# --train-prefix data/en-fr/raw/train \
-# --valid-prefix data/en-fr/raw/valid \
-# --test-prefix data/en-fr/raw/test \
-# --tiny-train-prefix data/en-fr/raw/tiny_train \
-# --input data/en-fr/raw/train.fr data/en-fr/raw/train.en \
-# --output data/en-fr/raw/bpe_codes \
-# --output-bpe data/en-fr/raw/train.bpe.fr data/en-fr/raw/train.bpe.en \
-# --vocabulary data/en-fr/raw/vocab.fr data/en-fr/raw/vocab.en \
-# --write-vocabulary data/en-fr/raw/vocab.fr data/en-fr/raw/vocab.en \
-# --threshold-src 1 --threshold-tgt 1 \
-# --num-words-src 4000 --num-words-tgt 4000 \
-# --symbols 4000 --train-bpe --dropout 0.1 --num-workers 16
